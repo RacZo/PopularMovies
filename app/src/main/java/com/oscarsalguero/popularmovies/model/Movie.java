e2ebf09fa8adcb308cce78f159b2818dab2cdade
Oscar S <osg@realactionsoft.com>
2016-11-18 23:00:13 -0500

    Added more Movie model object properties

--- app/src/main/java/com/oscarsalguero/popularmovies/model/Movie.java
+++ app/src/main/java/com/oscarsalguero/popularmovies/model/Movie.java
@@ -1,5 +1,8 @@
 package com.oscarsalguero.popularmovies.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
 /**
@@ -7,9 +10,7 @@ import com.google.gson.annotations.SerializedName;
  * Created by RacZo on 11/17/16.
  */
 
-public class Movie { // implements Parcelable {
-
-    private long id;
+public class Movie implements Parcelable {
 
     @SerializedName("poster_path")
     private String posterPath;
@@ -24,7 +25,25 @@ public class Movie { // implements Parcelable {
     @SerializedName("original_title")
     private String originalTitle;
 
-    /*
+    private long id;
+
+    @SerializedName("original_language")
+    private String originalLanguage;
+
+    private String title;
+
+    @SerializedName("backdrop_path")
+    private int backdropPath;
+
+    private float popularity;
+
+    @SerializedName("vote_count")
+    private int voteCount;
+
+    private boolean video;
+
+    @SerializedName("vote_average")
+    private float voteAverage;
 
     @Override
     public int describeContents() {
@@ -35,6 +54,4 @@ public class Movie { // implements Parcelable {
     public void writeToParcel(Parcel parcel, int i) {
 
     }
-
-    */
 }
